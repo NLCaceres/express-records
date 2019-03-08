1ed2c5ce2cb043935e7a48135202092732c500e8
NLCaceres <NLCaceres21@Gmail.com>
2019-03-08 15:18:01 -0800

    Changed app.js settings to deploy to heroku

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const catalogRouter = require('./routes/records');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/records");
 
 const app = express();
 
 // Import the mongoose module
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Set up default mongoose connection
-const mongoDB = 'mongodb://NLCaceres:9MivZcw9@ds031108.mlab.com:31108/local-records';
+const dev_db_url =
+  "mongodb://NCaceres:zVSby3uZZ6KP6Sv@ds031108.mlab.com:31108/local-records";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB);
 // Get Mongoose to use the global promise library
 mongoose.Promise = global.Promise;
@@ -22,21 +24,21 @@ mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 
 // Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/records', catalogRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/records", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -47,11 +49,11 @@ app.use((req, res, next) => {
 app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
